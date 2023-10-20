7789341eca07231633b098ca6e9006e8923e4270
stevestriker <stever185@gmail.com>
2023-10-20 15:44:54 +0200

    Gsap Completed Needs Tweaking

--- src/Components/Saranda.js
+++ src/Components/Saranda.js
@@ -7,21 +7,69 @@ import building5 from '../Imgs/Building5.png'
 import building6 from '../Imgs/Building6.png'
 import sea from '../Imgs/Sea.png'
 import sky from '../Imgs/Sky.png'
-import sun from '../Imgs/sun.png'
+import { useRef, useEffect, useLayoutEffect} from 'react'
+import { gsap } from 'gsap'
 import './Saranda.css'
 
 const Saranda = () => {
 
+  const tl = useRef(); //Timeline
+  const b1 = useRef(); // The Buildings 
+  const b2 = useRef();
+  const b3 = useRef();
+  const b4 = useRef();
+  const b5 = useRef();
+  const b6 = useRef();
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      tl.current = gsap.timeline({ defaults: { duration: .10, ease: "power2.out"} });
+
+      tl.current.from('#B1', {yPercent:100})
+                .from('#B2', {yPercent:100})
+                .from('#B3', {yPercent:100})
+                .from('#B4', {yPercent:100})
+                .from('#B5', {yPercent:-100})
+                .from('#B6', {yPercent:-100})
+                .from('#Sky', {yPercent:-100, duration:.45, ease:'bounce'})
+                .from('#Sea', {opacity: 0, duration:6}, .15, "seaFadeIn")
+      
+        // gsap.from(b1.current, {
+        //   duration:.35, yPercent:100, ease:'power4.out'
+        // })
+        // gsap.from(b2.current, {
+        //   duration:.25, yPercent:100, ease:'power4.out'
+        // })
+        // gsap.from(b3.current, {
+        //   duration:.3, yPercent:100, ease:'power4.out'
+        // })
+        // gsap.from(b4.current, {
+        //   duration:.53, yPercent:-100, ease:'power4.out'
+        // })
+        // gsap.from(b5.current, {
+        //   duration:.73, yPercent:-100, ease:'power4.out'
+        // })
+        // gsap.from(b6.current, {
+        //   duration:.93, yPercent:-100, ease:'power4.out'
+        // })
+
+    }, )  // <- IMPORTANT! Scopes selector text
+
+    return () =>  ctx.revert();
+
+  }, [])
+
+
   return (
     <div className="saranda-wrapper">
-        <img src={building1} alt='teeth' id="B1"/>
-        <img src={building2} alt='teeth' id="B2"/>
-        <img src={building3} alt='teeth' id="B3"/>
-        <img src={building4} alt='teeth' id="B4"/>
-        <img src={building5} alt='teeth' id="B5"/>
-        <img src={building6} alt='teeth' id="B6"/>
-        <img src={sea} alt='teeth' id="Sea"/>
-        <img src={sky} alt='teeth' id="Sky"/>
+        <img src={building1} className='building' ref={b1}alt='' id="B1"/>
+        <img src={building2} className='building' ref={b2} alt='' id="B2"/>
+        <img src={building3} className='building' ref={b3} alt='' id="B3"/>
+        <img src={building4} className='building' ref={b4} alt='' id="B4"/>
+        <img src={building5} className='building' ref={b5} alt='' id="B5"/>
+        <img src={building6} className='building' ref={b6} alt='' id="B6"/>
+        <img src={sea} alt='' id="Sea"/>
+        <img src={sky} alt='' id="Sky"/>
     </div>
   )
 }
